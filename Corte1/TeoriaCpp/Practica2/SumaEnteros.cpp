0fe592b2a4ead0c5971fec3c465cf02c0bad0c3d
L4nBurguesa <l4nburguesa@gmail.com>
2026-02-11 15:48:38 -0500

    Add files via upload

---
+++ Corte1/TeoriaCpp/Practica2/SumaEnteros.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int num1, num2, num3;
+
+	cout << "Ingrese el primer número:";
+	cin >> num1;
+
+	cout << "Ingrese el segundo número:";
+	cin >> num2;
+
+	num3 = num1 + num2;
+
+	cout << "La suma es:" << suma << endl;
+
+	return 0;
+}
